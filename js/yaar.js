09144828d339473017cda54aa4ba3ddf2286aaf4
FernandoHerbas <feer15.herbas@gmail.com>
2019-11-25 23:32:24 -0300

    Agrego un prototipo Pirata y otro prototipo BusquedaDelTesoro que hereda el comportamiento de Mision

--- js/yaar.js
+++ js/yaar.js
@@ -1,8 +1,33 @@
+/************************************ Misiones **************************************/
 function Mision() {
     this.esRealizablePorUnBarco = function (unBarco){
         return unBarco.tieneSuficienteTripulacion();
     }
 }
+function BusquedaDelTesoro(){
+    Mision.call(this); // heredo de mision
+
+    this.esUtil = function(unPirata){
+        return tieneAlgunItemObligatorio(unPirata) && unPirata.cantidadDeMonedas() <= 5; 
+        //;
+    }
+    var tieneAlgunItemObligatorio = function(unPirata){
+        return ["brújula", "mapa", "grogXD"].some(unPirata.tiene); //Se comporta como el any, no es necesario mandar el parametro, js infiere
+    }                                                              //que es un elemento de la coleccion
+}
+/************************************ Pirata **************************************/
+function Pirata(items,Monedas){
+
+    this.items = items;
+    this.Monedas = Monedas;
+    this.tiene = function(item){
+        return items.includes(item);
+    }
+    this.cantidadDeMonedas = function(){
+            return this.Monedas;
+    }
+}
+/************************************ Barco **************************************/
 function Barco(capacidad,tripulantes){
     this.capacidad = capacidad; //this: es una referencia al objeto desde que se llama la funcion.
     this.tripulantes = tripulantes;
@@ -15,14 +40,11 @@ function Barco(capacidad,tripulantes){
 }
 var barco = new Barco(2, ["pedro","lucas"]);
 var unaMision = new Mision();
+var unPirata = new Pirata(["grogXD"],4);
+
+BusquedaDelTesoro.prototype = Object.create(Mision.prototype);
+BusquedaDelTesoro.prototype.constructor = BusquedaDelTesoro;
+var unaBusqueda = new BusquedaDelTesoro();
 
 console.log(unaMision.esRealizablePorUnBarco(barco));
-/*
-function Tutor (nombre){
-    this.nombre = nombre;
-    this.saludar = function(){
-        console.log("hola a todos soy " + this.nombre);
-    }
-}
-var fer = new Tutor("Fer");
-fer.saludar();*/
+console.log(unaBusqueda.esUtil(unPirata));
